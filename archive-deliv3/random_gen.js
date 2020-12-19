5544c02f593a32d8fe7956d8369a3a15ae8c6919
alex-cen <alex-cen@users.noreply.github.com>
2020-12-18 21:27:40 -0500

    Deleting previous folder name

--- archive-deliv3/random_gen.js
+++
@@ -1,8 +0,0 @@
-randomNum = Math.round(Math.random()+1)
-window.onload = function(){
-    if (randomNum == 1) {
-        document.getElementById("case1").style.display="inline";
-    } else {
-        document.getElementById("case2").style.display="inline";
-    }
-}
